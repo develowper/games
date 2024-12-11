677f367479ebf2c77cb0c518ad2dd1cc619f2673
MMR <moj2raj2@gmail.com>
2024-12-11 16:27:07 +0330

    init adonis

--- app/services/helper_service.ts
+++ app/services/helper_service.ts
@@ -51,7 +51,7 @@ class Helper {
     telegram: 'https://t.me/support_paris',
   }
 
-  public static SOCKET_LINK = 'http://172.16.6.2:9204' ?? `${env.get('HOST')}:${env.get('PORT')}`
+  public static SOCKET_LINK = /*'http://172.16.6.2:9204' ??*/ `${env.get('HOST')}:${env.get('PORT')}`
   public static ERROR_STATUS = 400
   public static BANK = 'zarinpal'
   public static APP_VERSION = 1
