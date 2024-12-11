ae544e39456900709b7be6e5edda1103b09dce2f
MMR <moj2raj2@gmail.com>
2024-12-11 16:31:52 +0330

    init adonis

--- inertia/pages/home.vue
+++ inertia/pages/home.vue
@@ -1,40 +1,36 @@
 <script setup lang="ts">
-import {router} from '@inertiajs/vue3'
+import { router } from '@inertiajs/vue3'
 
-import {Head, usePage} from '@inertiajs/vue3'
-import {io} from 'socket.io-client'
-import {route} from '@izzyjs/route/client'
-import {onMounted, ref, toRef} from "vue";
-import {useHelper} from '~/composables/helper'
+import { Head, usePage } from '@inertiajs/vue3'
+import { io } from 'socket.io-client'
+import { route } from '@izzyjs/route/client'
+import { onMounted, ref, toRef } from 'vue'
+import { useHelper } from '~/composables/helper'
 
 const props = defineProps<{
-  prop1: string,
+  prop1: string
   errors?: Record<string, string[]>
 }>()
 
-const {__} = useHelper();
+const { __ } = useHelper()
 const renderCount = ref(0)
 const errors = usePage().props.errors
 
 const url = route('home') // /users/1
 
-const socket = io()
-socket.on('news', (data) => {
-  console.log(data)
-  socket.emit('my other event', {my: 'data'})
-})
-
+// const socket = io()
+// socket.on('news', (data) => {
+//   console.log(data)
+//   socket.emit('my other event', {my: 'data'})
+// })
 
 onMounted(() => {
   // console.log(usePage().props)
-
 })
-
-
 </script>
 
 <template>
-  <Head title="Homepage"/>
+  <Head title="Homepage" />
   <div class="">
     {{ __('greeting') }}
     <!--        {{ $inertia.page.props.language['greeting']}}-->
@@ -42,5 +38,4 @@ onMounted(() => {
 
     <div v-if="errors && errors.fullName">{{ errors.fullName.join(', ') }}</div>
   </div>
-
 </template>
