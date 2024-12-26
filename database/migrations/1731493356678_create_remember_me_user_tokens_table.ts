4e7dcc583d1f6d55a9b67f69e60e7f207e1af9c1
MMR <moj2raj2@gmail.com>
2024-12-26 17:50:09 +0330

    init adonis

--- database/migrations/1731493356678_create_remember_me_user_tokens_table.ts
+++ database/migrations/1731493356678_create_remember_me_user_tokens_table.ts
@@ -1,7 +1,7 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'remember_me_admin_tokens'
+  protected tableName = 'remember_me_user_tokens'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
