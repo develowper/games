1f7683c7536ad2c5721e1dde48b54e025940a293
MMR <moj2raj2@gmail.com>
2025-01-07 01:57:21 +0330

    init adonis

--- app/controllers/api/user_controller.ts
+++ app/controllers/api/user_controller.ts
@@ -72,7 +72,7 @@ export default class UserController {
       })
     const data = await request.validateUsing(registerValidator)
 
-    const user = await User.create({ ...data, agencyId: 1, agencyLevel: 0, isActive: 1 })
+    const user = await User.create({ ...data, agencyId: 1, agencyLevel: 0, isActive: true })
     const tokenData = await User.accessTokens.create(user)
     await UserFinancial.create({ userId: user.id, balance: 0 })
     return response.json({
