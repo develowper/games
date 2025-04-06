850b49c896530d2be794da6d52cb4cb7916c77af
MMR <moj2raj2@gmail.com>
2025-04-06 20:47:04 +0330

    init adonis

--- app/models/daberna.ts
+++ app/models/daberna.ts
@@ -385,7 +385,7 @@ export default class Daberna extends BaseModel {
       logText +=
         `🧍🏼‍♂️ بازیکنان: ${players
           .map((i: any) => {
-            return `${i.username}[${i.card_count}]`
+            return `${i.username}[${i.card_count}${i.user_role == 'us' ? '👤' : '🤖'}]`
           })
           .join('➖')}` + '\n'
       logText +=
