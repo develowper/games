a36cd7844f1bc2eb6b1c1d2241d77095e36db28b
MMR <moj2raj2@gmail.com>
2025-04-08 03:49:25 +0330

    init adonis

--- app/models/dooz.ts
+++ app/models/dooz.ts
@@ -58,10 +58,10 @@ export default class Dooz extends BaseModel {
     const updatedAt = room?.updatedAt
     const diff =
       updatedAt?.plus({ seconds: room.maxSeconds })?.diff(DateTime.now(), 'seconds').seconds ?? 0
-    console.log('room players', roomPlayers.count())
+    // console.log('room players', roomPlayers.count())
     console.log('room user players', roomPlayers.where('user_role', 'us').count())
     //checks users connected to socket
-    console.log('diff', diff)
+    // console.log('diff', diff)
     if (roomPlayers.count() < 2 || realPlayersCount == 0 || (realPlayersCount == 1 && diff > 0))
       return
     const roomSockets = await SocketIo.wsIo?.in(`room-${room.type}`).fetchSockets()
